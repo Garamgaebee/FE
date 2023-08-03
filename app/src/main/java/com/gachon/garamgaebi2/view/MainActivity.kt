289cabb2dff595f8cb80b650de802dedc292d3b3
1109min <1109min@naver.com>
2023-08-03 12:58:56 +0900

    Feat: First Screen

--- app/src/main/java/com/gachon/garamgaebi2/view/MainActivity.kt
+++ app/src/main/java/com/gachon/garamgaebi2/view/MainActivity.kt
@@ -3,10 +3,14 @@ package com.gachon.garamgaebi2.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gachon.garamgaebi2.R
+import com.gachon.garamgaebi2.base.BaseActivity
+import com.gachon.garamgaebi2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
+
 }
