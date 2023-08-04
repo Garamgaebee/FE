90346adb965cce59dd3cbe8119f3817a1dae25b8
1109min <1109min@naver.com>
2023-08-04 15:49:53 +0900

    Feat: Set Register String

---
+++ app/src/main/java/com/gachon/garamgaebi2/view/Register1EmailFragment.kt
@@ -0,0 +1,16 @@
+package com.gachon.garamgaebi2.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gachon.garamgaebi2.R
+import com.gachon.garamgaebi2.base.BaseBindingFragment
+import com.gachon.garamgaebi2.databinding.FragmentRegister1EmailBinding
+
+class Register1EmailFragment  : BaseBindingFragment<FragmentRegister1EmailBinding>(R.layout.fragment_register1_email) {
+
+
+}
+
