9fc9f169cae238a3e74dd77b35d6224b60151c53
1109min <1109min@naver.com>
2023-09-04 16:44:21 +0900

    Feat: 커뮤니티 프로필 편집 activity, layout 사진 추가기능

--- app/src/main/java/com/gachon/garamgaebi2/viewModel/CommunityProfileViewModel.kt
+++ app/src/main/java/com/gachon/garamgaebi2/viewModel/CommunityProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.gachon.garamgaebi2.viewModel
 
+import android.net.Uri
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -25,6 +26,38 @@ class CommunityProfileViewModel : ViewModel(){
     private val _withdrawalBtnClicked = MutableLiveData<Boolean>(false)
     val withdrawalBtnClicked : LiveData<Boolean> = _withdrawalBtnClicked
 
+
+    private val _allInputIsValid = MutableLiveData<Boolean>(false)
+    val allInputIsValid : LiveData<Boolean> = _allInputIsValid
+
+    private val _nameIsValid = MutableLiveData<Boolean>(false)
+    val nameIsValid : LiveData<Boolean> = _nameIsValid
+
+    private val _descriptionIsValid = MutableLiveData<Boolean>(false)
+    val descriptionIsValid : LiveData<Boolean> = _descriptionIsValid
+
+    private val _linkIsValid = MutableLiveData<Boolean>(false)
+    val linkIsValid : LiveData<Boolean> = _linkIsValid
+
+    val name = MutableLiveData<String>("")
+    val description = MutableLiveData<String>("")
+    val link = MutableLiveData<String>("")
+
+    private val _nameInputClicked = MutableLiveData<Boolean>(false)
+    val nameInputClicked : LiveData<Boolean> = _nameInputClicked
+
+    private val _descriptionInputClicked = MutableLiveData<Boolean>(false)
+    val descriptionInputClicked : LiveData<Boolean> = _descriptionInputClicked
+
+    private val _linkInputClicked = MutableLiveData<Boolean>(false)
+    val linkInputClicked : LiveData<Boolean> = _linkInputClicked
+
+
+    // 사진 업로드
+    val imageUri = MutableLiveData<Uri>()
+    val isLoadImage = MutableLiveData<Boolean>(false)
+
+
     fun onClickGoToWithdrawal(){
         _goToWithdrawalBtnClicked.value = true
     }
@@ -32,6 +65,43 @@ class CommunityProfileViewModel : ViewModel(){
     fun onClickWithdrawal(){
         _withdrawalBtnClicked.value = true
     }
+
+    fun onClickNameInput(){
+        _nameInputClicked.value = true
+        _descriptionInputClicked.value = false
+        _linkInputClicked.value = false
+    }
+    fun onClickDescriptionInput(){
+        _descriptionInputClicked.value = false
+        _descriptionInputClicked.value = true
+        _linkInputClicked.value = false
+    }
+
+    fun onClickLinkInput(){
+        _linkInputClicked.value = false
+        _descriptionInputClicked.value = false
+        _linkInputClicked.value = true
+    }
+
+    fun onNameTextChanged(s: CharSequence, start: Int, before: Int, count: Int){
+        _nameIsValid.value = s.isNotEmpty() && count <= 28
+        _allInputIsValid.value = _nameIsValid.value == true && _descriptionIsValid.value == true && _linkIsValid.value == true
+
+    }
+
+    fun onDescriptionTextChanged(s: CharSequence, start: Int, before: Int, count: Int){
+        _descriptionIsValid.value = s.isNotEmpty() && count <= 80
+        _allInputIsValid.value = _nameIsValid.value == true && _descriptionIsValid.value == true && _linkIsValid.value == true
+
+    }
+
+
+    fun onLinkTextChanged(s: CharSequence, start: Int, before: Int, count: Int){
+        _linkIsValid.value = s.isNotEmpty()
+        _allInputIsValid.value = _nameIsValid.value == true && _descriptionIsValid.value == true && _linkIsValid.value == true
+
+    }
+
 
 
 
