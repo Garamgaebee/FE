92f070893f7cef6447f26a9fe111df737125b349
Dongwon Shin <edvedv@naver.com>
2023-09-06 19:54:34 +0900

    Feat : 답변 게시물 작성/ 공지 작성 레이아웃 구현

--- app/src/main/java/com/gachon/garamgaebi2/viewModel/WriteViewModel.kt
+++ app/src/main/java/com/gachon/garamgaebi2/viewModel/WriteViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class WritePostViewModel: ViewModel() {
+class WriteViewModel: ViewModel() {
     val postContent = MutableLiveData<String>("")
     val imageFirst = MutableLiveData<String>("")
     val imageSecond = MutableLiveData<String>("")
@@ -14,9 +14,7 @@ class WritePostViewModel: ViewModel() {
     private val _etCnt = MutableLiveData<String>("0")
     val etCnt : LiveData<String> = _etCnt
 
-
     val completeIsValid = MutableLiveData<Boolean>(false)
-
 
     fun onEditTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
         completeIsValid.value = !s.isNullOrEmpty() // || image 갯수 로직
