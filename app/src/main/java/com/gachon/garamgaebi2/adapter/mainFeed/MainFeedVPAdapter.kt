604b1d6c4ee77343a34f52b5f2312e21c4df3451
edv-Shin <edvedv@naver.com>
2023-09-11 11:05:01 +0900

    Feat : 게시글 더미데이터, itemDecoration 추가

--- app/src/main/java/com/gachon/garamgaebi2/adapter/mainFeed/MainFeedVPAdapter.kt
+++ app/src/main/java/com/gachon/garamgaebi2/adapter/mainFeed/MainFeedVPAdapter.kt
@@ -3,13 +3,15 @@ package com.gachon.garamgaebi2.adapter.mainFeed
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.gachon.garamgaebi2.views.mainFeed.MainFeedActivity
+import com.gachon.garamgaebi2.views.mainFeed.MainFeedLatestFragment
+import com.gachon.garamgaebi2.views.mainFeed.MainFeedPopularityFragment
 
 class MainFeedVPAdapter(fragment: MainFeedActivity) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int = 2
     override fun createFragment(position: Int): Fragment {
         return when(position) {
-            0 -> Fragment()
-            1 -> Fragment()
+            0 -> MainFeedLatestFragment()
+            1 -> MainFeedPopularityFragment()
             else -> Fragment()
         }
     }
--- app/src/main/java/com/gachon/garamgaebi2/views/mainFeed/MainFeedPopularityFragment.kt
+++ app/src/main/java/com/gachon/garamgaebi2/views/mainFeed/MainFeedPopularityFragment.kt
@@ -2,7 +2,9 @@ package com.gachon.garamgaebi2.views.mainFeed
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gachon.garamgaebi2.R
+import com.gachon.garamgaebi2.adapter.mainFeed.MainFeedRVAdapter
 import com.gachon.garamgaebi2.base.BaseBindingFragment
 import com.gachon.garamgaebi2.databinding.FragmentMainFeedPopularityBinding
 
@@ -11,12 +13,23 @@ class MainFeedPopularityFragment : BaseBindingFragment<FragmentMainFeedPopularit
         super.onViewCreated(view, savedInstanceState)
         initView()
         initListener()
+        initRecyclerView()
     }
     override fun initView() {
         super.initView()
+
     }
 
     override fun initListener() {
         super.initListener()
+    }
+    private fun initRecyclerView() {
+        binding.fragmentMainFeedPopularityRv.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = MainFeedRVAdapter(listOf("no_image", "one_image", "second_image", "more_image"))
+            addItemDecoration(MainFeedRVItemDecoration(
+                0.5 * context.resources.displayMetrics.density,
+                resources.getColor(R.color.light_gray, null)))
+        }
     }
 }
