652bbd6bc1d86889b6dea8dea306b2f27d6e81ec
1109min <1109min@naver.com>
2023-09-21 13:37:21 +0900

    Feat: Side Menu Connection

--- app/src/main/java/com/gachon/garamgaebi2/views/mainFeed/MainFeedActivity.kt
+++ app/src/main/java/com/gachon/garamgaebi2/views/mainFeed/MainFeedActivity.kt
@@ -3,42 +3,80 @@ package com.gachon.garamgaebi2.views.mainFeed
 import android.content.Intent
 import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.gachon.garamgaebi2.R
 import com.gachon.garamgaebi2.adapter.mainFeed.MainFeedVPAdapter
 import com.gachon.garamgaebi2.base.BaseActivity
-import com.gachon.garamgaebi2.databinding.ActivityMainFeedBinding
+import com.gachon.garamgaebi2.databinding.MainMenuDrawerBinding
 import com.gachon.garamgaebi2.views.write.WritePostActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-class MainFeedActivity : BaseActivity<ActivityMainFeedBinding>(ActivityMainFeedBinding::inflate) {
+
+class MainFeedActivity : BaseActivity<MainMenuDrawerBinding>(MainMenuDrawerBinding::inflate) {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         initToolbar()
         initListener()
         initViewPager()
+
     }
     override fun initView() {
+        with(binding) {
+            myProfileBtn.btnLayout.setBackgroundResource(0)
+            myProfileBtn.btnIv.setImageResource(R.drawable.ic_my_profile)
+            myCommunityBtn.btnIv.setImageResource(R.drawable.ic_my_community)
+            allCommunityBtn.btnIv.setImageResource(R.drawable.ic_all_community)
+            makeCommunityBtn.btnIv.setImageResource(R.drawable.ic_plus_circle)
+            settingsBtn.btnIv.setImageResource(R.drawable.ic_settings)
+
+        }
+
     }
     private fun initToolbar() {
-        with(binding.toolbar) {
+        setSupportActionBar(binding.mainContentLayout.toolbar)
+        with(binding.mainContentLayout.toolbar) {
             logo = null
         }
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(com.gachon.garamgaebi2.R.drawable.ic_hamburger_menu)
+        binding.navigationView.itemIconTintList = null
+
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.mainDrawerLayout,
+            binding.mainContentLayout.toolbar,
+            R.string.my_community,
+            R.string.all_community
+        )
+        binding.mainDrawerLayout.addDrawerListener(actionBarDrawerToggle)
     }
+
     fun initListener() {
-        with(binding) {
+
+
+        with(binding.mainContentLayout) {
             icPostingCv.setOnClickListener {
+                Log.d("MainFeedActivity", "icPostingCv clicked")
+
                 startActivity(Intent(this@MainFeedActivity, WritePostActivity::class.java))
+                binding.mainDrawerLayout.openDrawer(GravityCompat.START)
+
             }
             searchIv.setOnClickListener {
 
             }
-            hamburgerMenuIv.setOnClickListener {
-
-            }
             activityMainFeedTl.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab) {
-                    val transitionDrawable = binding.tabLayoutBottomBorder.background as TransitionDrawable
+                    val transitionDrawable = binding.mainContentLayout.tabLayoutBottomBorder.background as TransitionDrawable
                     when (tab.position) {
                         0 -> transitionDrawable.startTransition(300)
                         1 -> transitionDrawable.reverseTransition(300)
@@ -53,10 +91,10 @@ class MainFeedActivity : BaseActivity<ActivityMainFeedBinding>(ActivityMainFeedB
     }
     private fun initViewPager() {
         val mainFeedVPAdapter = MainFeedVPAdapter(this)
-        binding.activityMainFeedVp.adapter = mainFeedVPAdapter
-        binding.activityMainFeedVp.isUserInputEnabled = false
+        binding.mainContentLayout.activityMainFeedVp.adapter = mainFeedVPAdapter
+        binding.mainContentLayout.activityMainFeedVp.isUserInputEnabled = false
         val tabNameArray = arrayListOf("최신순","인기순")
-        TabLayoutMediator(binding.activityMainFeedTl, binding.activityMainFeedVp) {
+        TabLayoutMediator(binding.mainContentLayout.activityMainFeedTl, binding.mainContentLayout.activityMainFeedVp) {
                 tab, position -> tab.text = tabNameArray[position]
         }.attach()
     }
